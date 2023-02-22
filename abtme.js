66957f069828dad5a24d4acb9f4930a10ef2687a
Kristian Dotson <KDotson387@gmail.com>
2023-02-22 08:48:21 -0600

    Update abtme.js

--- abtme.js
+++ abtme.js
@@ -1,9 +1,12 @@
 function quiz () {
    let qs = ["Is it true I want to get into game development",
-"Is it true I graduted highschool in 2023",
-"Is is false that i hate software developing",
-"Is it true Kristian wants to work for a big company",
-"Is it true that Kristian graduates code crew in 2023"];
+   "Is it true I graduted highschool in 2023",
+
+   "Is is false that i hate software developing",
+
+   "Is it true Kristian wants to work for a big company",
+
+   "Is it true that Kristian graduates code crew in 2023"];
 
 let as = ["Yes",
 "No",
