7ad9ead7c541bad7ef3cfbec7333ba74ebbd35f9
Kristian Dotson <KDotson387@gmail.com>
2023-02-22 10:19:29 -0600

    Update abtme.js

--- abtme.js
+++ abtme.js
@@ -1,12 +1,15 @@
 let person = prompt("Enter your name")
 alert(`Nice to meet you ${person}!`)
 
+
 function quiz () {
-    
+    let x = 0
+
    let qs = prompt("Is it true I want to get into game development");
    let qslow = qs.toLowerCase()
-    if ( qslow === true )
-        alert("Correct!")
+    if ( qslow === true ){
+        alert("Correct!") 
+        x ++;}
     else {
         alert("incorrect")
     }
@@ -14,33 +17,44 @@ function quiz () {
    
    let edu = prompt("Is it true I graduted highschool in 2022");
    let edulow = edu.toLowerCase()
-    if (edulow === "true")
+    if (edulow === "true"){
      alert("Correct!")
+      x ++;}
    else {
      alert("incorrect")
    }
+   
 
    let sd = prompt("Is is false that i hate software developing");
    let sdlow = sd.toLowerCase()
-    if (sdlow === "true")
+    if (sdlow === "true"){
      alert("Correct")
+     x ++;}
    else {
      alert("incorrect")
    }
 
    let comp = prompt("Is it true Kristian wants to work for a big company");
    let complow = comp.toLowerCase()
-    if (complow === "true")
+    if (complow === "true"){
      alert("Correct")
+     x++;}
    else {
      alert("incorrect")
    }
 
    let grad = prompt("Is it true that Kristian graduates code crew in 2023");
    let gradlow = grad.toLowerCase()
-    if (gradlow === "true")
+    if (gradlow === "true"){
      alert("Correct")
+     x ++;}
+     //console.log(x)
+     //console.log("correct")
    else {
      alert("incorrect")
+     //console.log(x)
+     //console.log("incorrect")
    }
+
+alert("Yay!" + " " + person + "you scored a" + " " + "out of 5 questions")
 }
