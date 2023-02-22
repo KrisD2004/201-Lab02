7762e7bbb700aae8ed989ea3c502428bb7167bdd
Kristian Dotson <KDotson387@gmail.com>
2023-02-21 21:54:49 -0600

    Update abtme.js

--- abtme.js
+++ abtme.js
@@ -1,7 +1,15 @@
 function quiz () {
-    let Name = prompt("Hello, What is your name?")
-        alert(`Nice to meet you ${Name}! Hope your ready for my game!`)
-    let qs = ["",
-    "",
-]
+   let qs = ["",
+"",
+"",
+"",
+""];
+
+let as = ["",
+"",
+"",
+"",
+"",]
 }
+
+let person = prompt("Enter your name")
