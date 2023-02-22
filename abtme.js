e300e9093043b9110a7d04dc2cd2d4d3de43091e
Kristian Dotson <KDotson387@gmail.com>
2023-02-22 09:29:01 -0600

    Update abtme.js

--- abtme.js
+++ abtme.js
@@ -1,20 +1,45 @@
-function quiz () {
-   let qs = prompt("Is it true I want to get into game development");
-
-   prompt("Is it true I graduted highschool in 2023")
-
-   prompt("Is is false that i hate software developing"),
-
-   prompt("Is it true Kristian wants to work for a big company"),
-
-   prompt("Is it true that Kristian graduates code crew in 2023");
-
-let as = ["Yes",
-"No",
-"Yes",
-"Yes",
-"Yes",]
-}
-
 let person = prompt("Enter your name")
 alert(`Nice to meet you ${person}!`)
+
+function quiz () {
+    
+   let qs = prompt("Is it true I want to get into game development");
+   let qslow = qs.toLowerCase()
+    if ( qslow === true )
+        alert("Correct!")
+    else {
+        alert("incorrect")
+    }
+   
+   let edu = prompt("Is it true I graduted highschool in 2022");
+   let edulow = edu.toLowerCase()
+    if (qss === true )
+     alert("Correct!")
+   else {
+     alert("incorrect")
+   }
+
+   let sd = prompt("Is is false that i hate software developing");
+   let sdlow = sd.toLowerCase()
+    if (sd === true)
+     alert("Correct")
+   else {
+     alert("incorrect")
+   }
+
+   let comp = prompt("Is it true Kristian wants to work for a big company");
+   let complow = comp.toLowerCase()
+    if (comp === true)
+     alert("Correct")
+   else {
+     alert("incorrect")
+   }
+
+   let grad = prompt("Is it true that Kristian graduates code crew in 2023");
+   let gradlow = grad.toLowerCase()
+    if (grad === true)
+     alert("Correct")
+   else {
+     alert("incorrect")
+   }
+}
